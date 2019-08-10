0337cd592bd18797b0638bdc2ccfbb80aec1d703
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-10 20:02:08 +0000

    Deploy dev-launchers-sandbox/project__example-game to github.com/dev-launchers-sandbox/project__example-game.git:gh-pages

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -5,7 +5,19 @@ export default class Enemy extends Character {
   constructor(scene, x, y) {
     super(scene, x, y);
     this.attackVal = 10;
+    this.step = 1;
+    this.updateCounter = 0;
   }
 
-  update() {}
+  update() {
+    /*
+    this.updateCounter++;
+    this.updateCounter % 60;
+    this.x = this.x + this.step;
+    if (this.x > 50) this.step = this.step + -1;
+    else if (this.x < 0) this.step = this.step + 1;
+
+    console.log(this.step);
+    */
+  }
 }
--- classes/Vehicle.js
+++ classes/Vehicle.js
@@ -6,14 +6,16 @@ export default class Vehicle {
     this.x = x;
     this.y = y;
     this.gravity = 0;
-    this.friction = 0;
+    this.friction = 0.2;
     this.speed = speed;
 
     // Create the physics-based sprite that we will move around and animate
     this.sprite = scene.physics.add
       .sprite(x, y, "johnny", 0)
-      .setDrag(500, 0)
-      .setMaxVelocity(200, 400);
+      .setDrag(0, 0)
+      .setMaxVelocity(200, 400)
+      .setFriction(this.friction)
+      .setBounce(1, 0);
   }
 
   update() {}
