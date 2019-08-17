8e26bda3e8989399784144cf56f80bf701170d4e
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-08-17 13:54:39 -0500

    architecture change
    we changed the architecture of the game

--- classes/Vehicle.js
+++ classes/Vehicle.js
@@ -1,21 +1,24 @@
 import Phaser from "phaser";
 
-export default class Vehicle {
+export default class Vehicle extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, speed) {
+    super(scene, x, y, speed, "Vehicle");
     this.scene = scene;
     this.x = x;
     this.y = y;
     this.gravity = 0;
-    this.friction = 0.2;
+    this.friction = 10;
     this.speed = speed;
 
     // Create the physics-based sprite that we will move around and animate
     this.sprite = scene.physics.add
       .sprite(x, y, "johnny", 0)
-      .setDrag(0, 0)
+      .setDrag(100, 0)
       .setMaxVelocity(200, 400)
       .setFriction(this.friction)
-      .setBounce(1, 0);
+      .setCollideWorldBounds(true)
+
+      .setBounce(1.5, 0);
   }
 
   update() {}
