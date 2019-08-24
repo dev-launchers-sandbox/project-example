14c1c282e25f36ca34ebff3674c09ad26510c7c8
alearm246 <47065061+alearm246@users.noreply.github.com>
2019-08-24 13:40:30 -0500

    Changes to Character and Enemy class
    Created the moveTowards into the Character class so Enemy can inherit from it

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -7,17 +7,16 @@ export default class Enemy extends Character {
     this.attackVal = 10;
     this.step = 1;
     this.updateCounter = 0;
+    /*console.log(this.scene);*/
   }
 
   update() {
+    this.moveTowards(this.scene.vehicle.x, this.scene.vehicle.y);
     /*
     this.updateCounter++;
-    this.updateCounter % 60;
-    this.x = this.x + this.step;
-    if (this.x > 50) this.step = this.step + -1;
-    else if (this.x < 0) this.step = this.step + 1;
-
-    console.log(this.step);
+    if(this.updateCounter % 60){
+      console.log(this.x)
+    }
     */
   }
 }
