481946e8b090d594de2b9fc1e4a6b5ae1937672a
Kris Gano <46331884+pyxld-kris@users.noreply.github.com>
2019-09-08 15:20:31 -0500

    Rebasing master
    rebase

--- classes/Powerup.js
+++ classes/Powerup.js
@@ -10,9 +10,26 @@ export default class Powerup extends Phaser.Physics.Arcade.Sprite {
     // Create the physics-based sprite that we will move around and animate
     this.sprite = scene.physics.add
       .existing(this)
-      .setDrag(500, 0)
+      //.setDrag(500, 0)
       .setMaxVelocity(200, 400)
       .setCollideWorldBounds(true);
+  }
+
+  moveAway(targetX, targetY) {
+    if (this.x < targetX) {
+      this.x -= 0.5;
+    } else if (this.x > targetX) {
+      this.x += 0.5;
+    }
+    if (this.y < targetY) {
+      this.y -= 0.5;
+    } else if (this.y > targetY) {
+      this.y += 0.5;
+    }
+  }
+
+  activate() {
+    this.scene.enemy.moveAway(this.scene.player.x, this.scene.player.y);
   }
 
   update() {}
