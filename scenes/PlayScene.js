f194c3bf023bd3c05a6016e18cc8a345df19aba2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-24 06:01:07 +0000

    Deploy dev-launchers-sandbox/project__hungry-ghost to github.com/dev-launchers-sandbox/project__hungry-ghost.git:gh-pages

--- scenes/PlayScene.js
+++ scenes/PlayScene.js
@@ -4,6 +4,8 @@ import Enemy from "../classes/Enemy.js";
 import Cake from "../classes/Cake.js";
 import Powerup from "../classes/Powerup.js";
 import FinishLine from "../classes/FinishLine.js";
+import Obstacle from "../classes/Obstacle.js";
+import RandomDataPoints from "../classes/RandomDataPoints.js";
 
 export default class PlayScene extends Phaser.Scene {
   constructor() {
@@ -52,6 +54,19 @@ export default class PlayScene extends Phaser.Scene {
     this.powerup = new Powerup(this, 100, 5);
     this.finishLine = new FinishLine(this, 500, 100);
 
+    this.randomDataPointsGenerator = new RandomDataPoints();
+    const obstacleLocations = this.randomDataPointsGenerator.datapoints(
+      2,
+      this.game.config.width,
+      this.game.config.height
+    );
+    console.log("obstacleLocations ", obstacleLocations);
+    this.obstacles = [];
+    obstacleLocations.forEach(point => {
+      console.log("point", point);
+      this.obstacles.push(new Obstacle(this, point.x, point.y));
+    });
+
     this.player.setDepth(1);
 
     const camera = this.cameras.main;
@@ -75,6 +90,12 @@ export default class PlayScene extends Phaser.Scene {
     this.physics.add.collider(this.enemy, this.platforms);
     //enemy and vehicle collision
     this.physics.add.collider(this.enemy, this.cake, this.enemyAndCakeCallback);
+    //obstacles collisions
+    this.physics.add.collider(
+      this.cake,
+      this.obstacles,
+      this.vehicleAndObstacleCallback
+    );
 
     //player and finishline collision
     this.physics.add.collider(
@@ -93,6 +114,9 @@ export default class PlayScene extends Phaser.Scene {
   }
   playerAndFinishLineCallback(cake, finishLine) {
     finishLine.winning();
+  }
+  vehicleAndObstacleCallback(vehicle, obstacle) {
+    obstacle.playerLost();
   }
 
   update(time, delta) {
