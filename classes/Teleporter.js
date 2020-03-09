ffb304fc178a221ff1fa6663cf9970cba3b32100
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-09 02:40:13 +0000

    Deploy dev-launchers-sandbox/project__hungry-ghost to github.com/dev-launchers-sandbox/project__hungry-ghost.git:gh-pages

---
+++ classes/Teleporter.js
@@ -0,0 +1,32 @@
+import Phaser from "phaser";
+
+const COLOR = 0xff0000;
+
+export default class Teleporter extends Phaser.GameObjects.Rectangle {
+  constructor(scene, x, y, width, height, destinationX, destinationY) {
+    super(scene, x, y, width, height, COLOR);
+    this.scene = scene;
+    this.width = width;
+    this.height = height;
+    this.destinationX = destinationX;
+    this.destinationY = destinationY;
+
+    // Add this to the scene as a Phaser game object
+    scene.add.existing(this, true);
+    // Create the physics-based sprite that we will move around and animate
+
+    this.sprite = scene.physics.add.existing(this, true);
+    //.setDrag(250, 0)
+    //.setMaxVelocity(200, 400)
+    //.setCollideWorldBounds(true);
+  }
+  // when called it telepo
+  teleport(sprite) {
+    sprite.x = this.destinationX;
+    sprite.y = this.destinationY;
+  }
+
+  update() {}
+
+  destroy() {}
+}
